d3b3d290d648bb362f8f49cca9591dfbe469786d
Ruben <ruben.pogosyan95@gmail.com>
2022-12-03 15:49:16 +0400

    HomeWork6

---
+++ Task 41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+Console.WriteLine("Please Write Numbers with space");
+string[] M = Console.ReadLine().Split(" ");
+int[] Numbers = new int[M.Length];
+
+for (int i = 0; i < M.Length; i++)
+{
+    Numbers[i] = int.Parse(M[i]);
+}
+int ShowPositiveNumbers(int[] arr)
+{
+    int sum = 0;
+    foreach (var item in arr)
+    {
+        if (item > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+Console.WriteLine($"PositiveNumbers = {ShowPositiveNumbers(Numbers)}");
+
+
---
+++ Task 43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Please Write Values with space");
+string[] Num = Console.ReadLine().Split(" ");
+float b1 = Convert.ToInt64(Num[0]);
+float k1 = Convert.ToInt64(Num[1]);
+float b2 = Convert.ToInt64(Num[2]);
+float k2 = Convert.ToInt64(Num[3]);
+
+
+Console.WriteLine($"{(b2 - b1) / (k1 - k2)}, {k1 * ((b2 - b1) / (k1 - k2)) + b1}");
